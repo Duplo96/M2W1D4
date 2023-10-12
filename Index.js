3ecbcf17ab97b714ea1429a089db156cc98526b1
Tiziano Titone <titone1996@gmail.com>
2023-10-12 23:55:07 +0200

    Final result

--- Index.js
+++ Index.js
@@ -9,39 +9,53 @@ REGOLE
 
 /* ESERCIZIO 1
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+
+
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*  I datatype definiscono il formato di una variabile e si dividono in:
+Stringhe: Sono tutti quei dati scritti a caratteri compresi dalle " Qui definiamo la nostra ciotola"
+Numeri: Sono numeri scritti senza nessun contorno "La nostra ciotola ha 50g all'interno"
+Boolean: Possiamo riassumerlo in VERO o FALSO "C'è dello zucchero nella nostra ciotola?"
+Undefined: Il nostro dato non ha un valore specifico "non abbiamo dato alla nostra ciotola il valore di zucchero all'interno"
+Null: Il nostro dato è stato svuotato del suo valore di partenza ovvero abbiamo tolto dalla nostra ciotola lo zucchero di prima 
+Object: è un contenitore di proprietà ovvero di elementi che caratterizano l'elemento "Il documento d'identità è un object in quanto definisce le nostre generalità divise per proprietà"*/
 
 /* ESERCIZIO 2
  Descrivi cos'è un oggetto in JavaScript, con parole tue.
+
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*  Un' oggetto è un contenitore di proprietà, che caratterizzano l'elemento con un valore e un nome 
+ ES: Il nostro documento d'identità 
+ Nome: Pluto
+ Cognome: Pippo
+ Data di nascita: Quella del mio compleanno
+ Città: Roma */
 
 /* ESERCIZIO 3
  Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let somma = 2 + 2
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let x = 12 
 
 /* ESERCIZIO 5
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let name = "Tiziano"
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let sottrazione = x - 4 
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -50,4 +64,10 @@ REGOLE
  NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let name1 = "john"
+let name2 = "John"
+
+console.log("john" !== "John")
+
+console.log(name1 === name2.toLowerCase("name2"))
+
